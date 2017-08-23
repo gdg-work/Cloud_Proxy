8f3351f8161cd200c92a52b1fb37a5998e1ae04f
Dmitriy G. Golub <d.golub@hostco.ru>
2017-08-23 21:39:22 +0500

    now TWO VMs are created

--- README.md
+++ README.md
@@ -7,6 +7,50 @@ SSH:
 
 Язык Python, используются библиотеки boto3, json, botocore.
 
+Что вам понадобится:
+
+ - Учётная запись на Amazon-е, привязанная банковская карта для оплаты (счёт выставляют в конце месяца).
+ - Пользователь IAM с правами полного доступа в Amazon Elastic Cloud (EC2), из-под которого вы будет создавать виртуальные машины.
+ - Настроенная Амазоновская аутентификация по ключу. У меня в Linux это выглядит так:
+
+```
+$ cd ~/.aws
+
+$ ls -l 
+total 15
+-rw-r--r-- 1 user group 213 Jun 05 config
+-rw------- 1 user group 480 Jul 13 credentials
+```
+
+В файле config описания профилей, в файле credentials закрытые части ключей, он должен быть закрыт на чтение кому попало.
+
+```
+$ cat config
+
+[profile testAWS]
+region = eu-central-1
+output = json
+
+[default]
+region = eu-west-1
+output = json
+```
+
+Регион ищем в Амазоне.  Для создания прокси лучше иметь отдельный профиль в AWS, названный как-нибудь нейтрально.
+
+В файле credentials, конечно, ключи. Они получаются через консоль AWS, раздел IAM. Пример файла (ключи, конечно, "левые"):
+
+```
+[default]
+aws_access_key_id = SIHUEWD6OKDOAGYUWOK6
+aws_secret_access_key = CidubMidVoorOfbodsAgiacsyidBoojdysk!6Kna
+
+[testAWS]
+aws_access_key_id = NOKAJSHONTAFUDKACAF1
+aws_secret_access_key = miBliteicDyWotjoajerph6Od+ofJugVickAitUf
+```
+
+
 TODO:
  - поиск подходящего AMI ID
  - создание не одной, а ДВУХ машин в разных регионах с образованием цепочки прокси между ними (например, на второй /выходной/ машине
--- create_vm.py
+++ create_vm.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import boto3
-import botocore as bc
+# import botocore as bc
 import argparse as ap
 import yaml
 # import json
@@ -14,11 +15,15 @@ DEFAULT_CFG = """
 ssh:
     keydir: "."
 aws:
-    machine_name: Test-Host-For-Proxy
-    profile : vagrantLopa
-    datacenter : eu-west-1
     ami : "ami-060cde69"
+    profile : vagrantLopa
     instance_type : "t2.micro"
+    frontend:
+        machine_name: Test-Proxy-Frontend
+        datacenter : eu-west-1
+    backend:
+        machine_name: Test-Proxy-Backend
+        datacenter : eu-central-1
 tor:
     port : 9050
 proxy:
@@ -27,7 +32,7 @@ proxy:
     http:
         port: 8080
 """
-SECURITY_GROUP_NAME='aws_proxy'
+SECURITY_GROUP_NAME = 'aws_proxy'
 
 
 def msg(s_data):
@@ -43,7 +48,8 @@ def s_create_security_group(o_ec2_res):
     """
     # check if we have our security group available
     b_groupFound = False
-    # d_resp = o_ec2_res.describe_security_groups(GroupNames=[SECURITY_GROUP_NAME])
+    # d_resp = o_ec2_res.describe_security_groups(
+    #                GroupNames=[SECURITY_GROUP_NAME])
     for o_sg in o_ec2_res.security_groups.all():
         if o_sg.group_name == SECURITY_GROUP_NAME:
             b_groupFound = True
@@ -51,7 +57,8 @@ def s_create_security_group(o_ec2_res):
             break
 
     if b_groupFound:
-        msg('*DBG* o_assign_security_group: target SG found')
+        msg('*INFO* o_assign_security_group: target SG found')
+        pass
     else:
         # create a security group
         o_sg = o_ec2_res.create_security_group(
@@ -87,11 +94,76 @@ def s_create_security_group(o_ec2_res):
     return s_grpId
 
 
+def o_create_instance_internal(o_ec2res, d_instance_data):
+    """Create an AWS EC2 instance. Parameters:
+    1) EC2 resource object
+    2) Dictionary with instance data (see below)
+    Returns: instance object.
+
+    d_instance_data = {'blockdevs': [], 'ami_id': '', 'inst_type': '',
+                       'region': '', 'keyname': '', 'sg_id': '',
+                       'vm_name': ""}
+    """
+    l_tagsSpec = [{'ResourceType': 'instance',
+                   'Tags': [{'Key': 'Name',
+                             'Value': d_instance_data['vm_name']},
+                            {'Key': 'AutoCreated', 'Value': 'Yes'},
+                            {'Key': 'PartOfProxy', 'Value': 'Yes'}
+                            ]}]
+    try:
+        l_instance = o_ec2res.create_instances(
+                        InstanceType=d_instance_data['inst_type'],
+                        ImageId=d_instance_data['ami_id'],
+                        MinCount=1,
+                        MaxCount=1,
+                        KeyName=d_instance_data['keyname'],
+                        BlockDeviceMappings=d_instance_data['blockdevs'],
+                        SecurityGroupIds=[d_instance_data['sg_id']],
+                        # DryRun = True,
+                        TagSpecifications=l_tagsSpec,
+                    )
+        if len(l_instance) == 1:
+            o_instance = l_instance.pop()
+
+    except Exception as e:
+        msg("*ERR* o_create_instance_internal:" +
+            "Cannot create an instance, error below:")
+        msg(str(e))
+        o_instance = None
+    return o_instance
+
+
+def o_create_instance2(o_ec2res, o_cfg, d_keyPair, s_sg_id):
+    l_instances = []
+    ld_blockdevs = [{'DeviceName': '/dev/xvdh',
+                     'Ebs': {'DeleteOnTermination': True,
+                             'VolumeSize': 20, 'VolumeType': 'standard'}}]
+    # s_ami_id = o_cfg['aws']['ami']
+    # s_inst_type = o_cfg['aws']['instance_type']
+    # s_region = o_cfg['aws']['region']
+    for o_vm_data in (o_cfg['aws']['backend'], o_cfg['aws']['frontend']):
+        try:
+            d_data = {'blockdevs': ld_blockdevs,
+                      'ami_id': o_cfg['aws']['ami'],
+                      'inst_type': o_cfg['aws']['instance_type'],
+                      'region': o_vm_data['datacenter'],
+                      'vm_name': o_vm_data['machine_name'],
+                      'sg_id': s_sg_id,
+                      'keyname': d_keyPair['KeyName'],
+                      }
+            o_inst = o_create_instance_internal(o_ec2res, d_data)
+            l_instances.append(o_inst)
+        except Exception as e:
+            msg("*ERR* o_create_instance: Can't create instance, error below:")
+            msg(str(e))
+    return l_instances
+
+
 def o_create_instance(o_ec2res, o_cfg, d_keyPair, s_sg_id):
     ld_blockdevs = [{'DeviceName': '/dev/xvdh',
                      'Ebs': {'DeleteOnTermination': True,
                              'VolumeSize': 20, 'VolumeType': 'standard'}}]
-    s_ami_id = 'ami-060cde69'
+    s_ami_id = o_cfg['aws']['ami']
     # s_def_instanceType = 't2.micro'
     s_inst_type = o_cfg['aws']['instance_type']
     # s_region = o_cfg['aws']['region']
@@ -99,8 +171,9 @@ def o_create_instance(o_ec2res, o_cfg, d_keyPair, s_sg_id):
         l_tagsSpec = [{'ResourceType': 'instance',
                       'Tags': [
                           {'Key': 'Name',
-                           'Values': o_cfg['aws']['machine_name']
-                           }, ]}]
+                           'Value': o_cfg['aws']['machine_name']},
+                          {'Key': 'AutoCreated', 'Value': 'Yes'}
+                          ]}]
     else:
         l_tagsSpec = []
     o_instance = None
@@ -124,44 +197,66 @@ def o_create_instance(o_ec2res, o_cfg, d_keyPair, s_sg_id):
     return o_instance
 
 
-def b_launch_dryrun(o_ec2, s_id):
-    """ Test run of an instance, return True if success, False otherwise """
-    try:
-        o_ec2.start_instances(InstanceIds=[s_id], DryRun=True)
-    except bc.ClientError as e:
-        if 'DryRunOperation' not in str(e):
-            raise
-    return False
+def o_search_my_instance_id(o_ec2, o_cfg):
+    """Поиск instance id в заданном регионе, ID образа и некоторым тегам
+       (имени и ключу AutoCreated)
+    """
+    s_id = ''
+    return s_id
 
 
-def o_launch_VM(o_cfg):
-    o_vm = None
+def s_search_ami_id(o_ec2, o_cfg):
+    """Поиск инстанса Ubuntu LTS в заданном в конфигурации регионе"""
+    s_ami_id = "ami-060cde69"
+    l_filters = [
+            {'Name': 'image-type', 'Values': ['machine']},
+            {'Name': 'architecture', 'Values': ['x86_64']},
+            {'Name': 'name', 'Values': ['ubuntu/images/*trusty*']},
+            # {'Name': 'root-device-type', 'Values': ['ebs']},
+            {'Name': 'is-public', 'Values': ['true']},
+            # {'Name': '', 'Values': []},
+            ]
+    d_response = o_ec2.describe_images(
+            ExecutableUsers=['all'],
+            Filters=l_filters,
+            )
+    print(d_response)
+    for d_ami_description in d_response['Images']:
+        pass
+
+    raise
+    return s_ami_id
+
+
+def launch_VMs(o_cfg):
     o_ec2_res = None
     o_session = boto3.Session(profile_name=o_cfg['aws']['profile'])
     o_ec2 = o_session.client('ec2')
     if o_ec2:
         d_keyPair = o_generate_key(o_ec2, o_cfg)
         o_ec2_res = o_session.resource('ec2', use_ssl=True)
+        # o_cfg['aws']['ami'] = s_search_ami_id(o_ec2, o_cfg)
     if o_ec2_res:
         s_sg_id = s_create_security_group(o_ec2_res)
-        lo_instances = o_create_instance(o_ec2_res, o_cfg, d_keyPair, s_sg_id)
-    for o_instance in lo_instances:
-        msg("*DBG* CREATE successful, instance information:")
-        msg(str(o_instance))
-        msg('Launched an instance with ID "{}" and IP {}'.format(
-            o_instance.instance_id, o_instance.public_ip_address))
-        # We know that there should be only one instance
-        o_instance.wait_until_running(
-            Filters=[
-                {
-                    'Name': 'instance-id',
-                    'Value': o_instance.instance_id
-                }])
-    return o_vm
+        lo_instances = o_create_instance2(o_ec2_res, o_cfg, d_keyPair, s_sg_id)
+    if len(lo_instances) == 2:
+        for o_instance in lo_instances:
+            msg("*DBG* CREATE successful, instance information:")
+            msg(str(o_instance))
+            msg('*INF* Launched an instance with ID "{}" and IP {}'.format(
+                o_instance.instance_id, o_instance.public_ip_address))
+            # We know that there should be only one instance
+            o_instance.wait_until_running(
+                Filters=[
+                    {
+                        'Name': 'instance-id',
+                        'Values': [str(o_instance.instance_id)]   # list
+                    }])
+    return
 
 
 def o_generate_key(o_ec2, o_cfg):
-    """ Делает запрос на генерацию ключа, разбирает полученный ключ,
+    """Делает запрос на генерацию ключа, разбирает полученный ключ,
     сохраняет его в файл в текущем каталоге (или в заданном? Имя файла?
     Но файл точно нужен, для SSH потом)
     - имя файла можно сгенерировать уникальным (ami id + time,
@@ -176,13 +271,12 @@ def o_generate_key(o_ec2, o_cfg):
             o_ec2.delete_key_pair(KeyName=s_keyName)
     # create a new keypair with a given name
     d_key = o_ec2.create_key_pair(KeyName=s_keyName)
-    print('*DBG* generated key: ' + str(d_key))
+    # msg('*DBG* generated key: ' + str(d_key))
 
     # write the secret key to a file
     s_fn = os.path.join(o_cfg['ssh']['keydir'], s_keyName + '.pem')
     with open(s_fn, "w") as f_out:
         f_out.write(d_key['KeyMaterial'])
-
     return d_key
 
 
@@ -207,8 +301,8 @@ def fo_readConfig(ls_args):
                 try:
                     o_config = yaml.load(f_in)
                 except yaml.YAMLError as ex:
-                    msg("Error parsing configuration: {}".format(ex))
-                    msg('Will use default')
+                    msg("*ERR* Error parsing configuration: {}".format(ex))
+                    msg('*CONT* Will use default')
         except FileNotFoundError:
             msg("File {} not found, using default configuration".format(
                 s_cfg_filename))
@@ -225,5 +319,5 @@ if __name__ == "__main__":
     # read the configuration from a first file in CLI
     o_cfg = fo_readConfig(sys.argv)
     msg(o_cfg)
-    o_new_vm = o_launch_VM(o_cfg)
+    launch_VMs(o_cfg)
     exit()
